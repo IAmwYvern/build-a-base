6ee96e8dbdbce3e018568fe54f1738796b1aaaa5
IAmwYvern <50150016+IAmwYvern@users.noreply.github.com>
2020-02-01 05:54:23 -0800

    patch 1.0.5.5
    added some functionality to the inventory system however it is really fucking broken at the moment. just try to click on an item

--- GUI/InventoryGUI.gd
+++ GUI/InventoryGUI.gd
@@ -3,7 +3,9 @@ extends Control
 onready var ItemsContainer = $MarginContainer/TextureRect/MarginContainer/VBoxContainer/Items/GridContainer
 onready var BackpackContainer = $MarginContainer/TextureRect/MarginContainer/VBoxContainer/BackpackContainer/GridContainer
 
-onready var ItemButtonScene = preload("res://GUI/ItemButton.tscn")
+var items:Array = []
+
+onready var item_button_scene = preload("res://GUI/ItemButton.tscn")
 
 func _ready():
 	_init_item_slots()
@@ -12,12 +14,19 @@ func _ready():
 	
 func _init_item_slots():
 	for i in ItemsContainer.columns :
-		var item_button = ItemButtonScene.instance()
+		var item_button = item_button_scene.instance()
 		ItemsContainer.add_child(item_button)
 		item_button.set_item(Item.new(null, 0, null))
 		
 func _init_backpack_slots(rows):
 	for i in BackpackContainer.columns * rows:
-		var item_button = ItemButtonScene.instance()
+		var item_button = item_button_scene.instance()
 		BackpackContainer.add_child(item_button)
-		item_button.set_item(Item.new("sand", 10, load("res://items/sand_block_item.png")))
+		item_button.name = str(i)
+		item_button.clear()
+		if i < 5:
+			item_button.set_item(Item.new("sand", 10, load("res://assets/textures/items/sand_block_item.png")))
+			items.push_back(item_button.get_item())
+
+
+	
--- GUI/ItemButton.gd
+++ GUI/ItemButton.gd
@@ -1,11 +1,25 @@
 extends TextureButton
 
+var current_item
+var is_hovering = false
+
 signal quantity_changed(qty)
+signal item_set_hover(item)
 
 func set_item(Item):
 	_set_name(Item.name)
 	_set_qty(Item.quantity)
 	_set_texture(Item.texture)
+	current_item = Item
+	
+func get_item():
+	return current_item
+	
+func clear():
+	current_item = Item.new("", 0, null)
+	_set_name("")
+	_set_qty(0)
+	_set_texture(null)
 	
 func _set_name(name):
 	if not name == null:
@@ -19,9 +33,19 @@ func _set_texture(newTexture : Texture):
 #SIGNALS
 func _on_ItemButton_quantity_changed(qty):
 	if qty == 0 :
-		$QuantityLabel.text = " "
+		$QuantityLabel.text = ""
 func _on_ItemButton_mouse_entered():
-	$NameLabel.visible = true
+	if not $QuantityLabel.text == "" :
+		$NameLabel.visible = true
 func _on_ItemButton_mouse_exited():
-	$NameLabel.visible = false
-	print("ok boomer")
+	if not $QuantityLabel.text == "" :
+		$NameLabel.visible = false
+
+
+func _on_ItemButton_pressed():
+	if not is_hovering:
+		$ItemHover.set_item(current_item)
+		$ItemHover.visible = true
+		current_item = null
+		is_hovering = true
+	
